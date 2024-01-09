4decc8993e6be4dfc8d970b4734da21c23de9b38
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2024-01-09 18:10:00 +0300

    feat: add chat window shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Sidebar } from './components/Sidebar';
+import { ChatWindow } from './components/ChatWindow';
 
 export default function App() {
   return (
     <div className="app-shell">
       <Sidebar />
-      <main className="main">
-        <header className="header">New Chat</header>
-        <section className="content">
-          <p>Start by sending a message.</p>
-        </section>
-      </main>
+      <ChatWindow />
     </div>
   );
 }
---
+++ src/components/ChatWindow.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ChatWindow = () => {
+  return (
+    <div className="main">
+      <header className="header">New Chat</header>
+      <section className="content">
+        <p>Start by sending a message.</p>
+      </section>
+    </div>
+  );
+};
